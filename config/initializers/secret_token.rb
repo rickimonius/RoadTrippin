543d7a0ba75830a2555d9b83078e588dfab15e3a
rickimonius <rickchamplin@gmail.com>
2014-02-09 13:45:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoadTrippin::Application.config.secret_key_base = '71e5c94712e0b9e99c8a8cf0e040c0424c8e4cb07f3a6dd4e5d24c59409f6515d444ece2f74f274ee9bf3f8df75474851c1a4498036bd8564abf3db606983e3a'
